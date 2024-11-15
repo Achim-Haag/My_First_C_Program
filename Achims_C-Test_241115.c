1dca549b2608e57423b89dbd7c29c415d469c2d1
Achim-Haag <git@achimhaag.de>
2024-11-15 15:34:12 +0100

    241115 1534 AH Initial creation

---
+++ Achims_C-Test_241115.c
@@ -0,0 +1,19 @@
+/*
+    My first C program, derived from c't 25/2024 (8.11.24), page 66
+    I will type this page's contents step by step, amended by my own modifications
+*/
+
+// Declaration of external library functions used in this program
+#include <ctype.h>      // tolower()
+#include <stdbool.h>    // bool, true and false
+#include <stdio.h>      // stdin, getchar(), fgets()
+#include <stdlib.h>     // atoi(), srand(), rand()
+#include <string.h>     // strncmp(), strchr()
+#include <time.h>       // time()
+// #include "words.h"   // structure with data used by this program, must be defined locally - later !
+
+// program entry after call
+int main(int argc, char* argv[]) {
+
+ printf("\nWaiting for you debugging me,\nplease press Enter after debugging has ended\n");
+}
